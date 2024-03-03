c6d111d1ca4afaa43b4631ea6c5070871d74bdc8
Cynosure24 <139391583+Cynosure24@users.noreply.github.com>
2024-03-03 16:29:25 +0100

    update

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
-import { Navigation } from '../../Components/Navigation/Navigation.jsx';
+import { Navigation } from './Navigation/Navigation.jsx';
 
-const HomePage = lazy(() => import('../../pages/HomePage.jsx'));
-const MovieDetailsPage = lazy(() => import('../../pages/MovieDetailsPage.jsx'));
-const MoviesPage = lazy(() => import('../../pages/MoviesPage.jsx'));
-const NotFoundPage = lazy(() => import('../../pages/NotFoundPage.jsx'));
-const MovieCast = lazy(() => import('../MovieCast/MovieCast.jsx'));
-const MovieReviews = lazy(() => import('../MovieReviews/MovieReviews.jsx'));
+const HomePage = lazy(() => import('../pages/HomePage.jsx'));
+const MovieDetailsPage = lazy(() => import('../pages/MovieDetailsPage.jsx'));
+const MoviesPage = lazy(() => import('../pages/MoviesPage.jsx'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage.jsx'));
+const MovieCast = lazy(() => import('./MovieCast/MovieCast.jsx'));
+const MovieReviews = lazy(() => import('./MovieReviews/MovieReviews.jsx'));
 
 export const App = () => {
   return (
