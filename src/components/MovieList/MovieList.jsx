15a06a6ce2e9feb93d98e26ec1fb7a02ec99e9b1
Cynosure24 <139391583+Cynosure24@users.noreply.github.com>
2024-03-05 23:39:44 +0100

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,38 +1,27 @@
-import { NavLink } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import css from "./MovieList.module.css";
+const defaultImg =
+  "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
 
-export default function MoviesList({ error, movies, location }) {
-  const defaultImg =
-    "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
+export const MovieList = ({ movies }) => {
+  const location = useLocation();
   return (
-    <div>
-      {error ? (
-        <p>Error fetching data</p>
-      ) : (
-        <ul className={css.list}>
-          {movies.length > 0 &&
-            movies.map(({ id, title, poster_path, vote_average }) => (
-              <li key={id} className={css.card}>
-                <NavLink to={`/movies/${id}`} state={{ from: location }}>
-                  <img
-                    src={
-                      poster_path
-                        ? `https://image.tmdb.org/t/p/w500${poster_path}`
-                        : defaultImg
-                    }
-                    alt={title}
-                    width={200}
-                    className={css.img}
-                  />
-                  <div className={css.text}>
-                    <p className={css.movieTitle}>{title}</p>
-                    <p>Rate: {vote_average}</p>
-                  </div>
-                </NavLink>
-              </li>
-            ))}
-        </ul>
-      )}
-    </div>
+    <ul className={css.movieList}>
+      {movies.map((movie) => (
+        <li key={movie.id} className={css.thumb}>
+          <Link to={`/movies/${movie.id}`} state={location}>
+            <img
+              src={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                  : defaultImg
+              }
+              alt={movie.title}
+            ></img>
+            <p className={css.title}>{movie.title}</p>
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
-}
+};
